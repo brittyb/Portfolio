11815c1bb525789b1639119cc23058de9319b1b3
brittyb <brittanyjbarnes09@gmail.com>
2024-06-16 19:11:39 -0400

    scroll fade out

--- src/App.js
+++ src/App.js
@@ -3,37 +3,25 @@ import { createRoot } from 'react-dom/client'; // Import createRoot from react-d
 import './App.css';
 
 
-
-
-
-const SectionTitle = (props) => {
-  // State to track whether the scroll position has reached the target point
+const useScrollToTarget = () => {
   const [scrolledToTarget, setScrolledToTarget] = useState(false);
-
   const sectionRef = useRef(null);
-
-
 
   const handleScroll = () => {
     if (!sectionRef.current) return;
-  
+
     const scrollY = window.scrollY || document.documentElement.scrollTop;
+
     const sectionRect = sectionRef.current.getBoundingClientRect();
     
-    // Compare scrollY with sectionRect.top
-    if (scrollY >= sectionRect.top) {
+    // Compare scrollY with component scrollY
+    if (scrollY >= sectionRect.top && scrollY <= sectionRect.top + scrollY) {
       setScrolledToTarget(true);
     } else {
       setScrolledToTarget(false);
     }
   };
 
-
-  // Effect to update the section position after the component mounts
-
-
-
-  // Effect to add scroll event listener when the component mounts
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
 
@@ -41,11 +29,20 @@ const SectionTitle = (props) => {
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []); // Adding sectionPosition to ensure handleScroll uses the updated position
+  }, []);
+
+  return [scrolledToTarget, sectionRef];
+};
+
+
+
+const SectionTitle = (props) => {
+  // State to track whether the scroll position has reached the target point
+  const [scrolledToTarget, sectionRef] = useScrollToTarget();
 
   return (
-    <div ref={sectionRef} id={`${props.name}Heading`}  className={`sectionTitleDiv ${scrolledToTarget ? 'target-color' : 'default-color'}`}>
-      <h3 className={`${props.name}Title sectionTitle`}>{props.name}</h3>
+    <div ref={sectionRef} id={`${props.name}Heading`}  className={`section-title-div ${scrolledToTarget ? 'section-color' : 'background-color'}`}>
+      <h3 className={`${props.name}Title section-title`}>{props.name}</h3>
     </div>
   );
 };
@@ -63,11 +60,12 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 });
 
 function Skill(props){
+  const [scrolledToTarget, sectionRef] = useScrollToTarget();
   return (
     <>
-      <div className="skillDiv">
-        <img className="skillImage" src={props.link}></img>
-        <h3 className="skillName" >{props.name}</h3>
+      <div ref={sectionRef} className={`skill-div ${scrolledToTarget ? 'skill-color' : 'background-color'}`}>
+        <img className="skill-image" src={props.link}></img>
+        <h3 className="skill-name" >{props.name}</h3>
       </div>
     </>
   );
@@ -77,9 +75,9 @@ function Skill(props){
 function Project(props){
   return (
     <>
-      <div className="projectDiv">
-        <h3 className="projectName">{props.name}</h3>
-        <p className="projectDescription">{props.description}</p>
+      <div className="project-div">
+        <h3 className="project-name">{props.name}</h3>
+        <p className="project-description">{props.description}</p>
       </div>
     </>
   );
@@ -90,7 +88,7 @@ export default function App() {
   return (
     <main>
 
-      <div id="skills" className="skills sectionDiv">
+      <div id="skills" className="skills section-div">
       <SectionTitle name="Skills" position="700"/>
       </div>
       <div className="row">
@@ -112,7 +110,7 @@ export default function App() {
       </div>
       
 
-      <div id="projects" className="projects sectionDiv">
+      <div id="projects" className="projects section-div">
       <SectionTitle name="Projects" position="700"/>
       
       </div>
@@ -120,11 +118,16 @@ export default function App() {
       <div className="row">
         <Project name="Scrapbook" description="some stuff"></Project>
         <Project name="Escape from the Zeller Cellar" description="aaaaa"></Project>
+        
+      </div>
+
+      <div className="row">
         <Project name="Creative Commissions" description="aaaaaaaa"></Project>
+        <Project name="OpenGL Room" description="aaaaaaaa"></Project>
       </div>
 
       
-      <div id="contact" className="contact sectionDiv">
+      <div id="contact" className="contact section-div">
       <SectionTitle name="Contact" position="800"/>
       
       </div>
