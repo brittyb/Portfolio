da45c0aa61f19d538a74b134c63df51c4292efdf
brittyb <brittanyjbarnes09@gmail.com>
2024-06-16 21:58:10 -0400

    added resume section

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import './App.css';
 const useScrollToTarget = () => {
   const [scrolledToTarget, setScrolledToTarget] = useState(false);
   const sectionRef = useRef(null);
+  
 
   const handleScroll = () => {
     if (!sectionRef.current) return;
 
-    const scrollY = window.scrollY || document.documentElement.scrollTop;
-
     const sectionRect = sectionRef.current.getBoundingClientRect();
     
     // Compare scrollY with component scrollY
-    if (scrollY >= sectionRect.top && scrollY <= sectionRect.top + scrollY) {
+  
+    if (sectionRect.top > -150 && sectionRect.top < window.innerHeight) {
       setScrolledToTarget(true);
     } else {
       setScrolledToTarget(false);
@@ -38,10 +38,11 @@ const useScrollToTarget = () => {
 
 const SectionTitle = (props) => {
   // State to track whether the scroll position has reached the target point
+  
   const [scrolledToTarget, sectionRef] = useScrollToTarget();
 
   return (
-    <div ref={sectionRef} id={`${props.name}Heading`}  className={`section-title-div ${scrolledToTarget ? 'section-color' : 'background-color'}`}>
+    <div ref={sectionRef} id={`${props.name}Heading`}  className={`section-title-div ${scrolledToTarget ? 'foreground-opacity' : 'background-opacity'}`}>
       <h3 className={`${props.name}Title section-title`}>{props.name}</h3>
     </div>
   );
@@ -63,7 +64,7 @@ function Skill(props){
   const [scrolledToTarget, sectionRef] = useScrollToTarget();
   return (
     <>
-      <div ref={sectionRef} className={`skill-div ${scrolledToTarget ? 'skill-color' : 'background-color'}`}>
+      <div ref={sectionRef} className={`skill-div ${scrolledToTarget ? 'foreground-opacity' : 'background-opacity'}`}>
         <img className="skill-image" src={props.link}></img>
         <h3 className="skill-name" >{props.name}</h3>
       </div>
@@ -73,11 +74,18 @@ function Skill(props){
 }
 
 function Project(props){
+  const [scrolledToTarget, sectionRef] = useScrollToTarget();
   return (
     <>
-      <div className="project-div">
+     <div ref={sectionRef} className={`project-div ${scrolledToTarget ? 'foreground-opacity' : 'background-opacity'}`}>
         <h3 className="project-name">{props.name}</h3>
+        <img className="screenshot-image" src={props.imagePath}></img>
         <p className="project-description">{props.description}</p>
+        <a className="image-link" href={props.link} target="_blank">
+          <button className="link-button">
+            <img src="https://cdn0.iconfinder.com/data/icons/shift-logotypes/32/Github-512.png" className="link-image"></img>
+          </button>
+        </a>
       </div>
     </>
   );
@@ -89,7 +97,7 @@ export default function App() {
     <main>
 
       <div id="skills" className="skills section-div">
-      <SectionTitle name="Skills" position="700"/>
+      <SectionTitle name="Skills"/>
       </div>
       <div className="row">
       <Skill name="React" link="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"></Skill>
@@ -111,24 +119,40 @@ export default function App() {
       
 
       <div id="projects" className="projects section-div">
-      <SectionTitle name="Projects" position="700"/>
+      <SectionTitle name="Projects" />
       
       </div>
       
       <div className="row">
-        <Project name="Scrapbook" description="some stuff"></Project>
-        <Project name="Escape from the Zeller Cellar" description="aaaaa"></Project>
+        <Project name="Scrapbook" description="A website that lets couples document the dates they had together. Utilizes Google Gemini API
+         to recommend them date ideas with AI. Create dates with titles, images, dates, and descriptions to search for them and add them to albums. 
+         I was primarily responsible for the JavaScript and Firebase functionality, but I also
+         created HTML and CSS. " imagePath="scrapbook.png"></Project>
+        <Project name="Escape from the Zeller Cellar" description="A web-based escape room game. Escape from the evil wizard Zeller's
+         mysterious cellar. Race against the clock, gather clues, and use the objects you find to escape the Zeller Cellar. 
+         I contributed to the HTML, CSS, Java Servlets, and SQL queries of this project. Each action saves the state of the game 
+        in a SQL database" link="https://github.com/hgrimm000/ZellerCellar"
+        imagePath="zeller-cellar.png"></Project>
         
       </div>
 
       <div className="row">
-        <Project name="Creative Commissions" description="aaaaaaaa"></Project>
-        <Project name="OpenGL Room" description="aaaaaaaa"></Project>
+        <Project name="Creative Commissions" description="Android app that lets art patrons find digital artists to commission. 
+        Built using Android Studio, Kotlin, and Firebase. Search for artists through commission posts and request commissions." link="https://github.com/brittyb/CreativeComms"></Project>
+        <Project name="OpenGL Room" description="A room created using OpenGL that lets you walk around, change the tv channel, move the blinds
+        , turn on the table light, and look in the mirror. 3d models made in blender." link="https://github.com/brittyb/CS370-Fall2022"
+        imagePath="graphics-project.png"></Project>
       </div>
 
+      <div id="resume" className="resume section-div">
+        <SectionTitle name="Resume"/>
+        <a href="Resume April 2024.pdf" target="_blank">View Resume</a>
+        <a href="Resume April 2024.pdf" download="Resume Brittany Barnes">Download Resume</a>
+
+      </div>
       
       <div id="contact" className="contact section-div">
-      <SectionTitle name="Contact" position="800"/>
+      <SectionTitle name="Contact" />
       
       </div>
 
